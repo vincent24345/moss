1497c64560b1b1180b6379ecc6ca8ef0bafc208f
Vincent Nguyen <vincentn1102@gmail.com>
2024-10-14 23:56:33 +1030

    file1

---
+++ bubblesort.cpp
@@ -0,0 +1,40 @@
+// File: bubble_sort_basic.cpp
+#include <iostream>
+using namespace std;
+
+// Function to perform basic bubble sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    
+    return 0;
+}
